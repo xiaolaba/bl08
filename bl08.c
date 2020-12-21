3672f96b53f48debe1f16f73fce584f36f7d4f53
xiaolaba <xiao_laba_cn@yahoo.com>
2020-12-21 12:44:39 +0800

    Update bl08.c

--- bl08.c
+++ bl08.c
@@ -45,7 +45,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include <stdlib.h>
 #include <fcntl.h>
-#include <termios.h>
+//#include <termios.h>
 #include <stdarg.h>
 #include <ctype.h>
 #include <stdio.h>
@@ -59,7 +59,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 # include <sys/wait.h>
 # include <sys/ioctl.h>
-# include <sys/termios.h>
+//# include <sys/termios.h>
 
 #else
 
