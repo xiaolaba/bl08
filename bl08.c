307e4a3c00e0e842c0cfd63330d9b87eefc1e5d3
xiaolaba <xiao_laba_cn@yahoo.com>
2020-12-21 12:31:06 +0800

    Update bl08.c

--- bl08.c
+++ bl08.c
@@ -58,7 +58,8 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #if defined(__linux__)
 
 # include <sys/wait.h>
-# include <sys/ioctl.h> 
+# include <sys/ioctl.h>
+# include <sys/termios.h>
 
 #else
 
